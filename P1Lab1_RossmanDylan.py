8fcae00933d3f9ee12c4096652d10ae18ab36336
ChimeraOfFear <rossmand0161@student.faytechcc.edu>
2024-09-16 16:42:14 -0400

    Add files via upload
    First submission

---
+++ P1Lab1_RossmanDylan.py
@@ -0,0 +1,11 @@
+#Dylan Rossman
+#September 16, 2024
+#P1Lab1
+#Create a python program using Idle allowing the user to input their name
+
+
+print('Enter your first name:', end=' ')
+first_name = input()
+print('Enter your last name:', end=' ')
+last_name = input()
+print('Hello,', first_name, last_name + '!','Welcome to CTI-110')
